d6c574fe5afabac9791a7aeae8c712f001a797dc
unknown <bodkhesamadhan188@gmail.com>
2024-02-08 20:31:12 +0530

    server setup

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const express = require("express")
-const app = express()
+const cors = require("cors")
+require("dotenv").config({ path: "./.env" })
+const mongoose = require("mongoose")
 
-app.listen(5000, console.log("SERVER RUNNING"))
+
+mongoose.connect(process.env.MONGO_URL)
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/admin/", require("./routes/todoRoute"))
+
+mongoose.connection.once("open", () => {
+    console.log("MONGO CONNECTED")
+
+    app.listen(5000, console.log("SERVER RUNNING"))
+})
+
+
+
